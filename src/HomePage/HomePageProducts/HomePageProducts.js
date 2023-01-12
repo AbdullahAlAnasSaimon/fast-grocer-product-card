94429f52c35d4f24f3cbbc249a15bf5b17d86faf
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-12 18:55:02 +0600

    completed product card design and fetch api

--- src/HomePage/HomePageProducts/HomePageProducts.js
+++ src/HomePage/HomePageProducts/HomePageProducts.js
@@ -5,20 +5,20 @@ const HomePageProducts = () => {
   const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/products/vegetables`)
+    fetch(` https://fast-grocer-server.vercel.app/products/vegetables`)
       .then(res => res.json())
       .then(data => setProducts(data.allproducts))
   }, [])
 
   // event handler for showing product on home page
   const handleShowProducts = (category) => {
-    fetch(`http://localhost:5000/products/${category}`)
+    fetch(` https://fast-grocer-server.vercel.app/products/${category}`)
       .then(res => res.json())
       .then(data => setProducts(data.allproducts))
   }
 
   return (
-    <div>
+    <div className='bg-[#fbfff4db]'>
       <h2 className='text-3xl text-center font-bold'>Featured Products</h2>
       <div className='border-b-[1px] border-gray-200 my-5 md:my-3'>
         <ul className='w-8/12 md:w-5/12 mx-auto flex justify-between h-40px'>
@@ -26,7 +26,7 @@ const HomePageProducts = () => {
             <button onClick={() => handleShowProducts('vegetables')} className='border-b-2 border-transparent font-bold text-gray-500 hover:text-[#8ba73b] hover:border-b-2 hover:border-[#8ba73b] -mb-[1px] py-3 duration-300'>Vegetables</button>
           </li>
           <li className='inline-block'>
-            <button onClick={() => handleShowProducts('vegetables')} className='border-b-2 border-transparent font-bold text-gray-500 hover:text-[#8ba73b] hover:border-b-2 hover:border-[#8ba73b] -mb-[1px] py-3 duration-300'>Fruits</button>
+            <button onClick={() => handleShowProducts('fruits')} className='border-b-2 border-transparent font-bold text-gray-500 hover:text-[#8ba73b] hover:border-b-2 hover:border-[#8ba73b] -mb-[1px] py-3 duration-300'>Fruits</button>
           </li>
           <li className='inline-block'>
             <button onClick={() => handleShowProducts('vegetables')} className='border-b-2 border-transparent font-bold text-gray-500 hover:text-[#8ba73b] hover:border-b-2 hover:border-[#8ba73b] -mb-[1px] py-3 duration-300'>Cookings</button>
@@ -39,7 +39,7 @@ const HomePageProducts = () => {
           </li>
         </ul>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-11/12 mx-auto py-10'>
         {
           products?.map(product => <SingleProduct
             key={product?.id}
--- src/HomePage/HomePageProducts/SingleProduct/SingleProduct.js
+++ src/HomePage/HomePageProducts/SingleProduct/SingleProduct.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import {TbShoppingCartPlus} from 'react-icons/tb';
+import {AiFillHeart, AiFillStar} from 'react-icons/ai';
 
 const SingleProduct = ({ products }) => {
   console.log(products);
-  const { product_name, product_photo, product_mesurement, product_price, product_ratings } = products;
+  const { product_name, product_photo, product_mesurement, product_price } = products;
   return (
-    <div>
-      <div>
-        <img src={product_photo} alt="" />
+    <div className='bg-white shadow-lg rounded-md'>
+      <div className='h-225px'>
+        <img src={product_photo} alt="product img" className='mx-auto h-[225px] rounded-t-md'/>
       </div>
-      <div>
+      <div className='p-4'>
         <span className='flex items-center'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="#ffd000" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffd000" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-        </svg>
-        <p className=''>{product_ratings}</p>
+        <AiFillStar className='text-yellow-400'/>
+        <AiFillStar className='text-yellow-400'/>
+        <AiFillStar className='text-yellow-400'/>
+        <AiFillStar className='text-yellow-400'/>
+        <AiFillStar className='text-yellow-400'/>
         </span>
-        <p>{product_name}</p>
-        <p>± {product_mesurement}</p>
-        <p>৳ {product_price}</p>
+        <p className={`text-[17px] font-bold md:h-[60px] text-zinc-700 hover:underline cursor-pointer`}>{product_name}</p>
+        <p className='text-zinc-700 font-semibold'><small>±{product_mesurement}</small></p>
+        <p className='text-[#8ba73b] text-2xl font-bold'>৳{product_price}</p>
+        <div className='flex mt-5'>
+          <button className='w-full mr-2 bg-[#c4f296] hover:bg-[#b0f36e] duration-300 py-1 rounded-full flex items-center justify-center'><TbShoppingCartPlus className=' mr-2'/>Add to Cart</button>
+          <button className='bg-#8ba73b px-3 rounded-full bg-[#ff00001c] hover:bg-[#ff000044] duration-300 flex items-center justify-center'><AiFillHeart className='text-red-600 mr-2'/>Wishlist</button>
+        </div>
       </div>
     </div>
   );
